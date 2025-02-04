2c9cbab6cd8fecef3b2e269a64087ca553e00702
janvipatel7 <janvi72004@gmail.com>
2025-02-04 16:31:24 +0530

    first commit for include javascript files

---
+++ lecture-1/assets/js/index.js
@@ -0,0 +1,3 @@
+console.log("Vande bharat");
+
+alert("kuch bhiii");
---
+++ lecture-2/index.js
@@ -0,0 +1,8 @@
+let a = 10;// let cannot reinitialize
+ a = 30;//current value display
+
+var b = 70; // var can be initialize
+var b = 80;
+
+const pi= 3.14; // initialize value from starting
+console.log(b);
