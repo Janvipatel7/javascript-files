6f98cd3efd5c3ead3750c91e308bb237b65a5bd5
janvipatel7 <janvi72004@gmail.com>
2025-04-24 14:33:28 +0530

    crud form updated

--- practice/add-to-cart/assets/js/index.js
+++ practice/add-to-cart/assets/js/index.js
@@ -1,50 +1,79 @@
 let obj = [
-    
     {
-        name : "White Shoes",
+        id:1,
+        name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHrcsbVx5Uxg4olzqp_Wt961Zk0V-Nx8ZCvg&s",
-        text : "Nice color and style also casual shoes"
+        text: "Nice color and style also casual shoes"
     },
     {
-        name : "White Shoes",
+        id:2,
+        name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHLLTBbrBcYYkglGYPti86DY0qk05pGmgSmQ&s",
-        text : "Nice color and style also casual shoes"
+        text: "Nice color and style also casual shoes"
     },
     {
-        name : "White Shoes",
+        id:3,
+        name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_1Ik3ISSfDDCUOYylPHw2EYj3AJeowvPZUw&s",
-        text : "Nice color and style also casual shoes"
+        text: "Nice color and style also casual shoes"
     },
     {
-        name : "White Shoes",
+        id:4,
+        name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRssew7j-H96x-EXKW4eeGSCA4P3GUO2WaEOg&s",
-        text : "Nice color and style also casual shoes"
+        text: "Nice color and style also casual shoes"
     },
     {
-        name : "White Shoes",
+        id:5,
+        name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPKZShEok-bCop9YhO7HEbug_MrCrwnlwQhA&s",
-        text : "Nice color and style also casual shoes"
+        text: "Nice color and style also casual shoes"
     },
     {
-        name : "White Shoes",
+        id:6,
+        name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyDrjf428SdTifw_aoz7dftsGCr7OO-xSyhA&s",
-       text : "Nice color and style also casual shoes"
+        text: "Nice color and style also casual shoes"
     },
 ]
+let cartArr = JSON.parse(localStorage.getItem("obj")) || []
+let card = document.getElementById("card");
+let counter = document.getElementById("counter")
+function addtoCart(productId){
+    let contain = obj.find((item) =>{
+        return productId == item.id;
+    })
+   
+    let productIdx = cartArr.findIndex((item) =>{
+        return productId == item.id;
+    })
 
-let card = document.getElementById("#card");
+    if(productIdx != -1){
+        alert("heeeeeeeeee")
+    }else{
+        contain.quantity = 1;
+        cartArr.push(contain)
+        localStorage.setItem("obj" , JSON.stringify(cartArr))
+    }
+    counter.innerHTML = cartArr.length;
+}
 
-
+counter.innerHTML = cartArr.length ;
 obj.forEach((item) => {
     card.innerHTML += `
-    <div>
-        <img src="${obj.image}" alt="${obj.name}">
-            <div class="card-body">
-                <h5 class="card-title"> ${obj.name} </h5>
-                    <p class="card-text"> ${obj.text} </P>
-                    <button class="btn btn-warning" onclick=addtoCart(${item.id})
-                         Add To Cart
-                    </button>
-            </div>
-    </div>`
+                <div class="col-4">
+                        <div class="card" style="width: 18rem;" >
+                            <div>
+                                <img src="${item.image}" alt="${item.name}">
+                                    <div class="card-body">
+                                        <h5 class="card-title"> ${item.name} </h5>
+                                            <p class="card-text"> ${item.text} </P>
+                                            <button class="btn btn-warning" onclick="addtoCart(${item.id})">
+                                                 Add To Cart
+                                            </button>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+   `
 })
