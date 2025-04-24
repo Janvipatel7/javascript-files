c22e7c30ef122dbd5e6c3eaad4093b3cd5ade969
janvipatel7 <janvi72004@gmail.com>
2025-04-24 14:38:30 +0530

    practice

--- practice/add-to-cart/assets/js/index.js
+++ practice/add-to-cart/assets/js/index.js
@@ -1,36 +1,36 @@
 let obj = [
     {
-        id:1,
+        id: 1,
         name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHrcsbVx5Uxg4olzqp_Wt961Zk0V-Nx8ZCvg&s",
         text: "Nice color and style also casual shoes"
     },
     {
-        id:2,
+        id: 2,
         name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHLLTBbrBcYYkglGYPti86DY0qk05pGmgSmQ&s",
         text: "Nice color and style also casual shoes"
     },
     {
-        id:3,
+        id: 3,
         name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_1Ik3ISSfDDCUOYylPHw2EYj3AJeowvPZUw&s",
         text: "Nice color and style also casual shoes"
     },
     {
-        id:4,
+        id: 4,
         name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRssew7j-H96x-EXKW4eeGSCA4P3GUO2WaEOg&s",
         text: "Nice color and style also casual shoes"
     },
     {
-        id:5,
+        id: 5,
         name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPKZShEok-bCop9YhO7HEbug_MrCrwnlwQhA&s",
         text: "Nice color and style also casual shoes"
     },
     {
-        id:6,
+        id: 6,
         name: "White Shoes",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyDrjf428SdTifw_aoz7dftsGCr7OO-xSyhA&s",
         text: "Nice color and style also casual shoes"
@@ -39,26 +39,26 @@ let obj = [
 let cartArr = JSON.parse(localStorage.getItem("obj")) || []
 let card = document.getElementById("card");
 let counter = document.getElementById("counter")
-function addtoCart(productId){
-    let contain = obj.find((item) =>{
-        return productId == item.id;
-    })
-   
-    let productIdx = cartArr.findIndex((item) =>{
+function addtoCart(productId) {
+    let contain = obj.find((item) => {
         return productId == item.id;
     })
 
-    if(productIdx != -1){
+    let productIdx = cartArr.findIndex((item) => {
+        return productId == item.id;
+    })
+
+    if (productIdx != -1) {
         alert("heeeeeeeeee")
-    }else{
+    } else {
         contain.quantity = 1;
         cartArr.push(contain)
-        localStorage.setItem("obj" , JSON.stringify(cartArr))
+        localStorage.setItem("obj", JSON.stringify(cartArr))
     }
     counter.innerHTML = cartArr.length;
 }
 
-counter.innerHTML = cartArr.length ;
+counter.innerHTML = cartArr.length;
 obj.forEach((item) => {
     card.innerHTML += `
                 <div class="col-4">
