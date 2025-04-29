a58db9d10916c5f5df57881ed572c63a763c1979
janvipatel7 <janvi72004@gmail.com>
2025-04-29 16:43:18 +0530

    added

--- practice/todo-list/assets/js/index.js
+++ practice/todo-list/assets/js/index.js
@@ -1,35 +1,81 @@
 let taskInput = document.getElementById("taskInput");
-    let selectInput = document.getElementById("selectInput");
-    let addTaskbtn = document.getElementById("addTask");
-    let tasklist = document.getElementById("tasklist");
+let selectInput = document.getElementById("selectInput");
+let addTask = document.getElementById("addTask");
+let tasklist = document.getElementById("tasklist");
+let istask;
+let editIdx;
 
-    addTaskbtn.addEventListener("click", () => {
-        if(taskInput.value === "" || selectInput.value === "-1"){
-            Swal.fire({
-                icon: "warning",
-                title: "Invalid Input!",
-                showConfirmButton: false,
-                timer: 1500,
-                backdrop: false
-            });
-        } else {
-            addNewTask();
+let arr = JSON.parse(localStorage.getItem("task")) || [];
+
+addTask.addEventListener("click" , ()=>{
+    if(istask){
+        let obj = {
+            taskInput : taskInput.value,
+            selectInput : selectInput.value,
         }
-    });
+        arr.splice(editIdx , 1 ,obj);
+        localStorage.setItem("task" , JSON.stringify(arr));
+        displayTask(arr);
+        taskInput.value = '';
+        selectInput.value= '-1';
+        istask = false;
+        return;
+    }
+    if(taskInput.value === "" || selectInput.value === ""){
+        Swal.fire({
+            icon: "warning",
+            title: "Invalid Input!",
+            showConfirmButton: false,
+            timer: 1500,
+            backdrop : false
+        });
+    }else{
+        let obj = {
+            taskInput : taskInput.value,
+            selectInput : selectInput.value,
+        }
+        arr.push(obj);
+        localStorage.setItem("task" , JSON.stringify(arr));
+        displayTask(arr);
+    }
+    taskInput.value = '';
+    selectInput.value= '-1';
 
-    function addNewTask(){
-        const taskTest = taskInput.value;
-        const priority = selectInput.value;
+})
 
+function displayTask(arr){
+    tasklist.innerHTML = ""
+    arr.forEach((item,idx)=>{
         tasklist.innerHTML += `
+            <div class="d-flex gap-5 py-4">
         <div class="d-flex gap-3">
-            <p>${taskTest}</p>
-            <span>${priority}</span>
+            <p>${item.taskInput}</p>
+            <span>${item.selectInput}</span>
         </div>
-        <div>
+        <div class="d-flex gap-2">
+            <button class="btn btn-warning p-2" >
+                <i class="bi bi-pencil-square" onclick="editTask(${idx})"></i>
+            </button>
             <button class="btn btn-danger p-2">
-                <i class="bi bi-trash"></i>
+                <i class="bi bi-trash" onclick="deleteTask(${idx})"></i>
             </button>
         </div>
-        `;
-    }
+        </div>
+        `
+    })
+}
+
+function deleteTask(idx){
+    arr.splice(idx , 1);
+    localStorage.setItem("task" , JSON.stringify(arr));
+    displayTask(arr);
+}
+
+function editTask(idx){
+    taskInput.value = arr[idx].taskInput;
+    selectInput.value = arr[idx].selectInput;
+    istask = true;
+    editIdx = idx
+}
+
+displayTask(arr)
